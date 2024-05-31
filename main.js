3083d607b4d0ca8ca6fd22872c195c5c2ffc404d
DevMisha18 <139760096+DevMisha18@users.noreply.github.com>
2024-06-01 01:36:40 +0200

    Grid 4x4 done

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ const divContainer = document.getElementsByClassName("grid-container")[0];
 
 const numOfSquares = 16;
 const side = divContainer.offsetWidth / Math.sqrt(numOfSquares);
+
+
+// Filling the grid with squares
 for (let i = 0; i < numOfSquares; i++) {
   let div = document.createElement("div");
 
   div.className = "square";
+  div.style.boxSizing = "border-box";
   div.style.border = "solid 2px black";
   [div.style.width, div.style.height] = [`${side}px`, `${side}px`];
 
